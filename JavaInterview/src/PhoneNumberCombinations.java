26f1b2ee0d2b651a6ee50da7c9034406a62a13b3
harshbits <hbhavsar2110@yahoo.com>
2018-06-13 00:23:26 -0500

    Letter Combinations of a Phone Number

--- JavaInterview/src/PhoneNumberCombinations.java
+++ JavaInterview/src/PhoneNumberCombinations.java
@@ -27,8 +27,18 @@ public class PhoneNumberCombinations {
 //	final static Character[][]
 
 	private static final String[] DICTIONARY = { "0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz",
-
 	};
+	
+	private static final char[][] CHAR_DICTIONARY = {{'0'}, 
+			{'1'},
+			{ 'a', 'b', 'c' },
+			{ 'd', 'e', 'f' },
+			{ 'g', 'h', 'i' },
+			{ 'j', 'k', 'l' },
+			{ 'm', 'n', 'o' },
+			{ 'p', 'q', 'r', 's' },
+			{ 't', 'u', 'v' },
+			{ 'w', 'x', 'y', 'z' }};
 	
 	public static void main(String[] args) {
 		
